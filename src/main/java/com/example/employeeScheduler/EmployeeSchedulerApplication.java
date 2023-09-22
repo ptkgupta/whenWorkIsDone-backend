bb6d2b671fea1d568fbc483ca9a931d02ce78809
ptkgupta <ptkiitk@gmail.com>
2023-09-23 04:43:40 +0530

    starting the java application and adding DB properties

---
+++ src/main/java/com/example/employeeScheduler/EmployeeSchedulerApplication.java
@@ -0,0 +1,13 @@
+package com.example.employeeScheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeSchedulerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeSchedulerApplication.class, args);
+	}
+
+}
